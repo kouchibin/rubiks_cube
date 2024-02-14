7cde978d86020c1a248f9c6b14c12ec193e2029e
kouchibin <kouchibin@gmail.com>
2024-02-14 11:45:20 +0800

    Add Face::transpose

--- src/main.rs
+++ src/main.rs
@@ -10,42 +10,62 @@ enum Color {
 use Color::*;
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
-struct Side([[Color; 3]; 3]);
+struct Face([[Color; 3]; 3]);
+
+impl Face {
+    fn transpose(&mut self) {
+       // 00->02, 01->12, 02->22;
+       // 10->01, 11->11, 12->21;
+       // 20->00, 21->10, 22->20
+       let original = self.clone();
+       for i in 0..=2 {
+        for j in 0..=2 {
+            self.0[j][2-i] = original.0[i][j];
+        }
+       }
+    }
+}
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 struct Cube {
     // White at the bottom. Red on the front.
-    y: Side,
-    b: Side,
-    r: Side,
-    w: Side,
-    o: Side,
-    g: Side,
+    y: Face,
+    b: Face,
+    r: Face,
+    w: Face,
+    o: Face,
+    g: Face,
 }
 
 impl Cube {
-    fn new(y: Side, b: Side, r: Side, w: Side, o: Side, g: Side) -> Self {
+    fn new(y: Face, b: Face, r: Face, w: Face, o: Face, g: Face) -> Self {
         Cube { y, b, r, w, o, g }
     }
 
     fn default() -> Self {
         Self::new(
-            Side([[Yellow; 3]; 3]),
-            Side([[Blue; 3]; 3]),
-            Side([[Red; 3]; 3]),
-            Side([[White; 3]; 3]),
-            Side([[Orange; 3]; 3]),
-            Side([[Green; 3]; 3]),
+            Face([[Yellow; 3]; 3]),
+            Face([[Blue; 3]; 3]),
+            Face([[Red; 3]; 3]),
+            Face([[White; 3]; 3]),
+            Face([[Orange; 3]; 3]),
+            Face([[Green; 3]; 3]),
         )
     }
 
     fn rotate_clockwise(&mut self, _color: Color) {
-
-        self.b = Side([[Red, Red, Red], [Blue, Blue, Blue], [Blue, Blue, Blue]]);
-        self.r = Side([[Green, Green, Green], [Red, Red, Red], [Red, Red, Red]]);
-        self.o = Side([[Blue, Blue, Blue], [Orange, Orange, Orange], [Orange, Orange, Orange]]);
-        self.g = Side([ [Orange, Orange, Orange], [Green, Green, Green], [Green, Green, Green]]);
-
+        self.b = Face([[Red, Red, Red], [Blue, Blue, Blue], [Blue, Blue, Blue]]);
+        self.r = Face([[Green, Green, Green], [Red, Red, Red], [Red, Red, Red]]);
+        self.o = Face([
+            [Blue, Blue, Blue],
+            [Orange, Orange, Orange],
+            [Orange, Orange, Orange],
+        ]);
+        self.g = Face([
+            [Orange, Orange, Orange],
+            [Green, Green, Green],
+            [Green, Green, Green],
+        ]);
     }
 }
 
@@ -56,26 +76,44 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_face_transpose() {
+        let mut face = Face([
+            [Orange, Orange, Orange],
+            [Green, Green, Green],
+            [Red, Red, Red],
+        ]);
+        face.transpose();
+        assert_eq!(
+            Face([
+                [Red, Green, Orange],
+                [Red, Green, Orange],
+                [Red, Green, Orange],
+            ]),
+            face
+        );
+    }
+
+    #[test]
     fn test_default_cube() {
         let default_cube = Cube::new(
-            Side([
+            Face([
                 [Yellow, Yellow, Yellow],
                 [Yellow, Yellow, Yellow],
                 [Yellow, Yellow, Yellow],
             ]),
-            Side([[Blue, Blue, Blue], [Blue, Blue, Blue], [Blue, Blue, Blue]]),
-            Side([[Red, Red, Red], [Red, Red, Red], [Red, Red, Red]]),
-            Side([
+            Face([[Blue, Blue, Blue], [Blue, Blue, Blue], [Blue, Blue, Blue]]),
+            Face([[Red, Red, Red], [Red, Red, Red], [Red, Red, Red]]),
+            Face([
                 [White, White, White],
                 [White, White, White],
                 [White, White, White],
             ]),
-            Side([
+            Face([
                 [Orange, Orange, Orange],
                 [Orange, Orange, Orange],
                 [Orange, Orange, Orange],
             ]),
-            Side([
+            Face([
                 [Green, Green, Green],
                 [Green, Green, Green],
                 [Green, Green, Green],
@@ -90,23 +128,24 @@ mod tests {
         cube.rotate_clockwise(Yellow);
 
         let expected_cube = Cube::new(
-            Side([
+            Face([
                 [Yellow, Yellow, Yellow],
                 [Yellow, Yellow, Yellow],
                 [Yellow, Yellow, Yellow],
             ]),
-            Side([[Red, Red, Red], [Blue, Blue, Blue], [Blue, Blue, Blue]]),
-            Side([[Green, Green, Green], [Red, Red, Red], [Red, Red, Red]]),
-            Side([
+            Face([[Red, Red, Red], [Blue, Blue, Blue], [Blue, Blue, Blue]]),
+            Face([[Green, Green, Green], [Red, Red, Red], [Red, Red, Red]]),
+            Face([
                 [White, White, White],
                 [White, White, White],
                 [White, White, White],
             ]),
-            Side([[Blue, Blue, Blue],
+            Face([
+                [Blue, Blue, Blue],
                 [Orange, Orange, Orange],
                 [Orange, Orange, Orange],
             ]),
-            Side([
+            Face([
                 [Orange, Orange, Orange],
                 [Green, Green, Green],
                 [Green, Green, Green],
@@ -120,24 +159,24 @@ mod tests {
         cube.rotate_clockwise(Blue);
 
         let expected_cube = Cube::new(
-            Side([
+            Face([
                 [Orange, Yellow, Yellow],
                 [Orange, Yellow, Yellow],
                 [Orange, Yellow, Yellow],
             ]),
-            Side([[Blue, Blue, Blue], [Blue, Blue, Blue], [Blue, Blue, Blue]]),
-            Side([[Yellow, Red, Red], [Yellow, Red, Red], [Yellow, Red, Red]]),
-            Side([
+            Face([[Blue, Blue, Blue], [Blue, Blue, Blue], [Blue, Blue, Blue]]),
+            Face([[Yellow, Red, Red], [Yellow, Red, Red], [Yellow, Red, Red]]),
+            Face([
                 [Red, White, White],
                 [Red, White, White],
                 [Red, White, White],
             ]),
-            Side([
+            Face([
                 [Orange, Orange, White],
                 [Orange, Orange, White],
                 [Orange, Orange, White],
             ]),
-            Side([
+            Face([
                 [Green, Green, Green],
                 [Green, Green, Green],
                 [Green, Green, Green],
